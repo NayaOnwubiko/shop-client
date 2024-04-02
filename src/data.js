32ad295badc95a01044b0b624a665cba26df81a9
Naya O <nayao@nayas-air.home>
2024-04-02 10:58:00 -0400

    vite configuration fixed

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 export const sliderItems = [
   {
     id: 1,
-    img: "https://cdn.shopify.com/s/files/1/1230/9376/products/ONL-SP3416008_7EMulti_20Colored.jpg?v=1677748474",
+    img: "https://i.ibb.co/cXFnLLV/3.png",
     title: "SUMMER SALE",
     desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
     bg: "f5fafd",
--- src/requestMethod.js
+++ src/requestMethod.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/";
+const BASE_URL = "http://localhost:8080/api/";
 const TOKEN =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0OTFlNjY1YjdlM2Q3YmI0ZTBlMWU5NyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY4NzU0NjgxOCwiZXhwIjoxNjg3ODA2MDE4fQ.bHBUgjeGfEnxh8C753C2d0qTOsBkKaKOP7Z4atTsLTA";
 
