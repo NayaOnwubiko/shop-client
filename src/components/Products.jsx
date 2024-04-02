a957a3b39c04330d392d128c75b3de8a95d5c60a
Naya O <nayao@nayas-air.home>
2024-04-02 11:45:14 -0400

    deployment config added

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -20,8 +20,8 @@ const Products = ({ cat, filters, sort }) => {
       try {
         const res = await axios.get(
           cat
-            ? `http://localhost:8080/api/products?category=${cat}`
-            : "http://localhost:8080/api/products"
+            ? `https://shop-api-a69h.onrender.com/api/products?category=${cat}`
+            : "https://shop-api-a69h.onrender.com/api/products"
         );
         setProducts(res.data);
       } catch (err) {}
